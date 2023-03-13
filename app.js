548f96ec4a85e839e9d57a3e930590a7561c2ff5
Christian <christian.caicedo96@gmail.com>
2023-03-13 18:59:31 -0500

    started working on nav button

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const venue = document.querySelector('#venue')
+const home = document.getElementById('#home')
 
-venue.onclick = function (){
 
+
+const travel = document.getElementById('#travel')
+
+
+const thingsToDo = document.getElementById('#thingsToDo')
+
+
+const photos = document.getElementById('#photos')
+
+
+const weddingParty = document.getElementById('#weddingParty')
+
+
+const registry = document.getElementById('#registry')
+
+
+const rsvp = document.getElementById('#rsvp')
+
+home.onclick = function() {
+    this.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToStory() {
+    const ourStory = document.getElementById('ourStory')
+    const ourStoryScroll = document.getElementById('ourStoryScroll')
+
+    ourStory.onclick = ()=> {
+        ourStoryScroll.scrollIntoView();
+    }
 }
