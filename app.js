d755c4775db043bb0bc7cb81090823b3df1fd54f
Christian <christian.caicedo96@gmail.com>
2023-03-28 22:31:27 -0500

    fixed navlinks

--- app.js
+++
@@ -1,9 +0,0 @@
-const ourStory = document.querySelector('#ourStory')
-const ourStoryScroll = document.querySelector('#ourStoryScroll')
-
-
-// ourStory.onclick = function() {
-//     const ourStoryScroll = document.querySelector('#ourStoryScroll')
-//     ourStoryScroll.scrollIntoView();
-//     console.log('tits')
-// }
